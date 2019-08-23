175af92baaa3d04270b86d456e1d254a4b5a9e4c
unknown <jasoncollum@gmail.com>
2019-08-23 12:23:04 -0700

    Book Model

--- BookShelfApp/Models/Book.cs
+++ BookShelfApp/Models/Book.cs
@@ -1,6 +1,34 @@
-﻿namespace BookShelfApp.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookShelfApp.Models
 {
     public class Book
     {
+        [Required]
+        [MinLength(10), MaxLength(13)]
+        public string ISBN { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Genre { get; set; }
+
+        [Required]
+        [Display(Name = "Publish Date")]
+        public DateTime PublishDate { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+
+        [Required]
+        public int AuthorId { get; set; }
+
+       [Required]
+        public int UserID { get; set; }
+
+        [Required]
+        public ApplicationUser Owner { get; set; }
     }
 }
