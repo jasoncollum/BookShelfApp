39ee599916d494ded3d8c2070820c6320e65a3cb
unknown <jasoncollum@gmail.com>
2019-08-23 11:15:11 -0700

    update InputModel, OnPostAsync, and Register.cshtml

---
+++ BookShelfApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using BookShelfApp.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BookShelfApp.Areas.Identity.IdentityHostingStartup))]
+namespace BookShelfApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
