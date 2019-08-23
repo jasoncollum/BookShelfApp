f3d7b8de18fe20c6fad01b3dd19eb1a8fbdbe026
unknown <jasoncollum@gmail.com>
2019-08-23 10:00:49 -0700

    update startup and LoginPartial

--- BookShelfApp/Startup.cs
+++ BookShelfApp/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.EntityFrameworkCore;
 using BookShelfApp.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using BookShelfApp.Models;
 
 namespace BookShelfApp
 {
@@ -38,7 +39,7 @@ namespace BookShelfApp
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>()
+            services.AddDefaultIdentity<ApplicationUser>()
                 .AddDefaultUI(UIFramework.Bootstrap4)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
